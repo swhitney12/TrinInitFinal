dc3a93e6e62846164a115380495dffe4974203a2
Sabrina <swhitney@trinity.edu>
2021-05-04 20:43:42 -0500

    database started

--- server/public/javascript/trininit.js
+++ server/public/javascript/trininit.js
@@ -49,6 +49,7 @@ class LoginComponent extends React.Component {
     }
 }
 
+
 class ProfileComponent extends React.Component {
     constructor(props) {
         super(props);
@@ -116,16 +117,16 @@ class ProfileComponent extends React.Component {
 
                     ce('div', {id: 'myProjSecListings'},
                         //code to insert projects here, template for what will be appended in foreach
-                        ce('div', {className: 'myProjListing'},
-                            ce('div', {className: 'myProjListingTitleDiv'},
-                                ce('h3', {className: 'myProjListingTitle'}, 'ProjectName'),
-                                ce('p', {className: 'myProjListingCreator'}, 'Created by rainihuynh'),
+                        ce('div', {className: 'ProjListing'},
+                            ce('div', {className: 'ProjListingTitleDiv'},
+                                ce('h3', {className: 'ProjListingTitle'}, 'ProjectName'),
+                                ce('p', {className: 'ProjListingCreator'}, 'Created by rainihuynh'),
                             ),
-                            ce('p', {className: 'myProjListingDesc'}, 'Brief Description that should be cut off after a few lines like this one. This is a super cool project idea that everyone should look into...'),
-                            ce('div', {className: 'myProjListingEngagementDiv'},
-                                ce('p', {className: 'myProjListingEngagementInfo'}, '10 Interested Collaborators'),
+                            ce('p', {className: 'ProjListingDesc'}, 'Brief Description that should be cut off after a few lines like this one. This is a super cool project idea that everyone should look into...'),
+                            ce('div', {className: 'ProjListingEngagementDiv'},
+                                ce('p', {className: 'ProjListingEngagementInfo'}, '10 Interested Collaborators'),
                                 ce('vl'),
-                                ce('p', {className: 'myProjListingEngagementInfo'}, '20 Comments')
+                                ce('p', {className: 'ProjListingEngagementInfo'}, '20 Comments')
                             )
                         )
                     )
@@ -175,71 +176,54 @@ class MainComponent extends React.Component {
                 ),
             ),
 
-            ce('div', {id: 'profileBody'},
-                ce('div', {id: 'leftProfileDiv'}, 
-                    ce('div', {id: 'leftProfileImageDiv'},
-                        ce('img', {id:'leftProfileImg', src: profImg})
-                    ),
-
-                    ce('div', {id:'leftProfileInfoDiv'},
-                        ce('div', {className: 'leftProfileInfoWrapper'},
-                            ce('h3', {className:'profileMainSubheader1'}, 'Username'),
-                            ce('h4', {className:'profileMainSubheader2'}, 'rainihuynh')
-                        ),
-                        ce('div', {className: 'leftProfileInfoWrapper'},
-                            ce('h3', {className:'profileMainSubheader1'}, 'Major'),
-                            ce('h4', {className:'profileMainSubheader2'}, 'Computer Science'),
+            ce('div', {id: 'mainBody'},
+                ce('div', {id: 'leftMainDiv'}, 
+                    ce('div', {id: 'myProjectsMainTitleDiv'},
+                        ce('div', {id:'myProjectsMainTitle'}, 
+                            ce('h3', {className: 'mainSubheader1'}, 'My Projects')
                         ),
 
-                        ce('div', {className: 'leftProfileInfoWrapper'},
-                            ce('h3', {className:'profileMainSubheader1'}, 'Graduation Year'),
-                            ce('h4', {className:'profileMainSubheader2'}, '2021'),
-                        ),
-
-                        ce('div', {className: 'leftProfileInfoWrapper'},
-                            ce('h3', {className:'profileMainSubheader1'}, 'GitHub'),
-                            ce('h4', {className:'profileMainSubheader2'}, 'rainihuynh')
+                        ce('div', {id: 'addProjectMainBtnDiv'},
+                            ce('button', {id: 'addProjectMainBtn'}, '+')
                         )
                     ),
 
-                    ce('div', {id:'editProfileBtnDiv'},
-                        ce('Button', {id: 'editProfileBtn'}, 'Edit Profile')
-                    )
-                ),
-
-                ce('div', {id: 'middleProfileDiv'},
-                    ce('div', {id: 'myProjectSecTitleDiv'},
-                        ce('h2', {id: 'myProjectSecTitle'}, 'My Projects')
+                    ce('div', {id: 'myProjectsMainListDiv'},
+                        //insert code here to populate this, use below as template
+                        ce('h4', {className: 'mainSubheader2'}, 'Project 1')
                     ),
 
-                    ce('div', {id: 'myProjSecListings'},
+                    ce('div', {id: 'likedProjectsMainTitleDiv'},
+                        ce('h3', {className: 'mainSubheader1'}, 'Liked Projects')            
+                    ),
+
+                    ce('div', {id: 'likedProjectsMainListDiv'},
+                        //insert code here to populate this, use below as a template
+                        ce('h4', {className: 'mainSubheader2'}, 'Liked Project 1')
+                    )
+
+
+                ),
+
+                ce('div', {id: 'rightMainDiv'},
+
+                    ce('div', {id: 'ProjSecListings'},
                         //code to insert projects here, template for what will be appended in foreach
-                        ce('div', {className: 'myProjListing'},
-                            ce('div', {className: 'myProjListingTitleDiv'},
-                                ce('h3', {className: 'myProjListingTitle'}, 'ProjectName'),
-                                ce('p', {className: 'myProjListingCreator'}, 'Created by rainihuynh'),
+                        ce('div', {className: 'ProjListing'},
+                            ce('div', {className: 'ProjListingTitleDiv'},
+                                ce('h3', {className: 'ProjListingTitle'}, 'ProjectName'),
+                                ce('p', {className: 'ProjListingCreator'}, 'Created by rainihuynh'),
                             ),
-                            ce('p', {className: 'myProjListingDesc'}, 'Brief Description that should be cut off after a few lines like this one. This is a super cool project idea that everyone should look into...'),
-                            ce('div', {className: 'myProjListingEngagementDiv'},
-                                ce('p', {className: 'myProjListingEngagementInfo'}, '10 Interested Collaborators'),
+                            ce('p', {className: 'ProjListingDesc'}, 'Brief Description that should be cut off after a few lines like this one. This is a super cool project idea that everyone should look into...'),
+                            ce('div', {className: 'ProjListingEngagementDiv'},
+                                ce('p', {className: 'ProjListingEngagementInfo'}, '10 Interested Collaborators'),
                                 ce('vl'),
-                                ce('p', {className: 'myProjListingEngagementInfo'}, '20 Comments')
+                                ce('p', {className: 'ProjListingEngagementInfo'}, '20 Comments')
                             )
                         )
                     )
                 ),
 
-                ce('div', {id: 'rightProfileDiv'},
-                    ce('div', {id: 'profileInterestsDiv'},
-                        ce('h3', {className:'profileMainSubheader3'}, 'Interests'),
-                        ce('h4', {className:'profileMainSubheader2'}, 'Frontend dev, data, visualization, sports, cybersecurity')
-                    ),
-
-                    ce('div', {id: 'profileSkillsDiv'},
-                        ce('h3', {className:'profileMainSubheader3'}, 'Skills'),
-                        ce('h4', {className:'profileMainSubheader2'}, 'Java, C/C++, React, Javascript, HTML, CSS')
-                    )
-                )
             )
         )
     }
