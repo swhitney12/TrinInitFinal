aa6705d0c761fa7676a8cba6dff1144757538a60
espradli <espradli@trinity.edu>
2021-05-05 12:23:44 -0500

    Begin the user model

--- server/app/controllers/Trininit.scala
+++ server/app/controllers/Trininit.scala
@@ -4,12 +4,22 @@ import javax.inject._
 
 import shared.SharedMessages
 import play.api.mvc._
+import models._
+
+import play.api.db.slick.DatabaseConfigProvider
+import scala.concurrent.ExecutionContext
+import play.api.db.slick.HasDatabaseConfigProvider
+import slick.jdbc.JdbcProfile
+import slick.jdbc.PostgresProfile.api._
+import scala.concurrent.Future
 
 @Singleton
-class Trininit @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
+class Trininit @Inject()(protected val dbConfigProvider: DatabaseConfigProvider, cc: ControllerComponents)(implicit ec: ExecutionContext) 
+    extends AbstractController(cc) with HasDatabaseConfigProvider[JdbcProfile] {
 
+  private val userModel = new UsersModel(db)
+  
   def trininitIndex = Action { implicit request =>
     Ok(views.html.trininit())
   }
-
 }
---
+++ server/app/models/UsersModel.scala
@@ -0,0 +1,47 @@
+package models
+
+import slick.jdbc.PostgresProfile.api._
+import scala.concurrent.ExecutionContext
+import models.Tables._
+import scala.concurrent.Future
+import org.mindrot.jbcrypt.BCrypt
+
+class UsersModel(db: Database)(implicit ec: ExecutionContext) {
+  /**
+    * Validates if a user is in the database
+    *
+    * @param username
+    * @param password
+    * @return the user's ID if the user is in the database
+    */
+  def validateUser(username: String, password: String): Future[Option[Int]] = {
+    val matches = db.run(Users.filter(userRow => userRow.username === username).result)
+    matches.map(userRows => userRows.headOption.flatMap {
+      userRow => if (BCrypt.checkpw(password, userRow.password)) Some(userRow.id) else None
+    })
+  }
+
+  /**
+    * Creates a user in the database
+    *
+    * @param user
+    * @return the new user's ID if creation was successful
+    */
+  def createUser(user: UserData): Future[Option[Int]] = {
+    val matches = db.run(Users.filter(userRow => userRow.username === user.username).result)
+    matches.flatMap { userRows =>
+      if (userRows.isEmpty) {
+        db.run(Users += UsersRow(-1, user.username, BCrypt.hashpw(user.password, BCrypt.gensalt()), 
+                                 user.major, user.graduationYear, user.githubLink))
+          .flatMap { addCount => 
+            if (addCount > 0) db.run(Users.filter(userRow => userRow.username === user.username).result)
+              .map(_.headOption.map(_.id))
+            else Future.successful(None)
+          }
+      } else Future.successful(None)
+    }
+  }
+}
+
+case class UserData(username: String, password: String, major: String,
+                    graduationYear: Int, githubLink: Option[String])
