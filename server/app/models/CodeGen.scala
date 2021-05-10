87f7e32f419c02d40b435a26c1d134fd48ea4c14
Kevin Crusius <kevin@Kevins-MBP.home>
2021-05-09 20:29:38 -0500

    pre work with sabrina

--- server/app/models/CodeGen.scala
+++ server/app/models/CodeGen.scala
@@ -4,8 +4,9 @@ object CodeGen extends App {
   slick.codegen.SourceCodeGenerator.run(
     "slick.jdbc.PostgresProfile",
     "org.postgresql.Driver",
-    "jdbc:postgresql://localhost/trininit?user=emersonspradling&password=password",
-    "/Users/emersonspradling/Developer/WebApps (3345)/TrinInitFinal/server/app",
+    "jdbc:postgresql://localhost/trininit?user=kcrusius&password=password",
+    "/Users/kevin/Desktop/webAppsFinalProj/TrinInitFinal/server/app",
     "models", None, None, true, false
   )
 }
+
